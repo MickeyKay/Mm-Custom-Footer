b2d4d619993896dff5d6e803372e428bf333cc23
Mickey Kay <mickey@mickeykaycreative.com>
2013-09-08 19:15:00 -0700

    Split PHP into separate files, finished functionality, removed unused files

--- mm-custom-footer.php
+++ mm-custom-footer.php
@@ -9,35 +9,27 @@
  * License:     GPLv2+
  */
 
-function mmcf_do_settings_page() {
-	add_options_page( 'MIGHTYminnow Custom Footer', 'Mm Custom Footer', 'manage_options', 'mm-custom-footer', 'mmcf_output_settings');
-}
-add_action('admin_menu', 'mmcf_do_settings_page');
+// Definitions
+define( 'PLUGIN_NAME', 'MIGHTYminnow Custom Footer' );
 
-function mmcf_output_settings() { ?>
-	<div class="wrap">
-	<?php screen_icon(); ?>
-	<h2>Your Plugin Page Title</h2>
-<?php }
+// Includes
+require_once dirname( __FILE__ ) . '/admin.php';
 
-// Enqueue custom jQuery file - that's it!
-function mmcf_do_custom_footer_jquery() {
-
-	// Load jQuery if it isn't already
-    wp_enqueue_script('jquery');
- 
-    // Load custom jQuery
-	wp_enqueue_script( 'mm-custom-footer', plugin_dir_url( __FILE__ ) . 'mm-custom-footer.js' );
+// Enqueue custom CSS
+function mmcf_scripts() {
 
 	// Load custom CSS
 	wp_enqueue_style( 'mm-custom-footer', plugin_dir_url( __FILE__ ) . 'mm-custom-footer.css' );
 
 }
-add_action( 'wp_enqueue_scripts', 'mmcf_do_custom_footer_jquery');
+add_action( 'wp_enqueue_scripts', 'mmcf_scripts');
 
 
 function mmcf_do_custom_footer() {
-	echo '<div class="mm-footer">Website made at a <a href="http://mightyminnow.com">MIGHTYminnow</a> Website Weekend course</div>';
+	if ( 'text' == get_option('mmcf-output-method') )
+		echo '<div class="mm-footer">Powered by <a href="http://mightyminnow.com" title="MIGHTYminnow Website Weekend">MIGHTYminnow Website Weekend</a></div>';
+	elseif ( 'image' == get_option('mmcf-output-method') )
+		echo '<div class="mm-footer">Powered by<br /><a href="http://mightyminnow.com" title="MIGHTYminnow Website Weekend"><img src="http://www.mightyminnow.com/wp-content/uploads/2013/07/website_weekend_icon.jpg" alt="MIGHTYminnow Website Weekend"</a></div>';
 }
 add_action( 'wp_footer', 'mmcf_do_custom_footer' );
 
